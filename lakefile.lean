274cfcbf2477ef7b3004ad2fa7cc8715c19dd4dc
Kim Morrison <kim@leanprover.com>
2026-07-31 11:15:45 +0000

    chore: bootstrap released repository

---
+++ lakefile.lean
@@ -0,0 +1,21 @@
+import Lake
+
+open System Lake DSL
+
+package «hex-berlekamp-zassenhaus» where
+  leanOptions := #[⟨`doc.verso, true⟩, ⟨`doc.verso.suggestions, false⟩]
+
+require HexBerlekamp from git
+  "https://github.com/leanprover/hex-berlekamp.git" @ "648b5dc96ff9d15f19c691df7200589b2205269b"
+
+require HexHensel from git
+  "https://github.com/leanprover/hex-hensel.git" @ "dc3d8ee06bf124993f4816d7952fe502fb23bd8e"
+
+require HexLLL from git
+  "https://github.com/leanprover/hex-lll.git" @ "a73f188bbd7ea48c4a1bb1e6d608b4f131026512"
+
+@[default_target]
+lean_lib HexBerlekampZassenhaus
+
+lean_lib HexBerlekampZassenhausTests where
+  globs := #[`HexBerlekampZassenhaus.FactorTacticTests]
